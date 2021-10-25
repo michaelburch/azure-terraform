6cd4ac9ebd435572101f6cd8bd00669e355ba2fc
Michael Burch <me@michaelburch.net>
2021-10-25 16:48:49 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # azure-terraform
 Basic Terraform Modules for Azure
+
+## Example usage
+
+module "vnet" {
+  source = "github.com/michaelburch/azure-terraform/modules/virtual_network"
+}
+
+## Target a specific branch
+
+module "vnet" {
+  source = "github.com/michaelburch/azure-terraform/modules/virtual_network?ref=main"
+}
---
+++ modules/event_hub/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_eventhub_namespace" "namespace" {
+  name                = var.namespace_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.sku
+  capacity            = var.capacity
+  tags                = var.tags
+}
+
+resource "azurerm_eventhub" "hub" {
+  for_each = { for hub in var.hubs : hub.name => hub }
+
+  name                                           = each.key
+  resource_group_name                            = var.resource_group_name
+  namespace_name                                 = azurerm_eventhub_namespace.namespace.name
+  partition_count                                = each.value.partition_count 
+  message_retention                              = each.value.message_retention
+
+}
